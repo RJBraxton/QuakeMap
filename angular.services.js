d67d3f5f4fdeee72e1c60d2f66148edaf64be25e
= <=>
2014-04-05 23:00:07 -0500

    conflict...

--- angular.services.js
+++ angular.services.js
@@ -13,33 +13,7 @@ quakemap.factory('q', function($rootScope, $http, $interval){
 				$scope.data = [];
 				for (i=0; i<data.features.length;i++){
 					$scope.data.push(
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> e05a35bfad9300bac546f437f0ceb8e818061f27
-						[data.features[i].properties.time, data.features[i].properties.mag, data.features[i].geometry.coordinates[2]]
-=======
 						[data.features[i].properties.time, data.features[i].properties.mag]
->>>>>>> parent of 38e8a8b... Flot-tooltips added
-=======
-						[data.features[i].properties.time, data.features[i].properties.mag]
->>>>>>> parent of 38e8a8b... Flot-tooltips added
-=======
-						[data.features[i].properties.time, data.features[i].properties.mag]
->>>>>>> parent of 38e8a8b... Flot-tooltips added
-<<<<<<< HEAD
-=======
-						[data.features[i].properties.time, data.features[i].properties.mag]
->>>>>>> parent of 38e8a8b... Flot-tooltips added
-=======
->>>>>>> e05a35bfad9300bac546f437f0ceb8e818061f27
-=======
-						[data.features[i].properties.time, data.features[i].properties.mag]
->>>>>>> parent of 38e8a8b... Flot-tooltips added
 						);
 				};
 				$scope.errorCheck(true);
@@ -97,14 +71,6 @@ quakemap.directive('chart', [function() {
     link: function(scope, elem, attrs) {
     	var scale = d3.scale.linear().domain([2.0,4.5,7.5,8.5]).range(["green","blue", "orange","red"])
     	var foo = scope[attrs.ngModel];
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> e05a35bfad9300bac546f437f0ceb8e818061f27
     	var opts = {
     		xaxis: {mode: 'time', autoscaleMargin: 0.05, minTickSize: [1, "day"]},
     		yaxis: {},
@@ -126,24 +92,7 @@ quakemap.directive('chart', [function() {
 					}
 				}
     	};
-=======
     	var opts = {xaxis: {mode: 'time', minTickSize: [1, "day"]}, yaxis: {}, points: { show: true, radius: 10, fill: true }};
->>>>>>> parent of 38e8a8b... Flot-tooltips added
-=======
-    	var opts = {xaxis: {mode: 'time', minTickSize: [1, "day"]}, yaxis: {}, points: { show: true, radius: 10, fill: true }};
->>>>>>> parent of 38e8a8b... Flot-tooltips added
-=======
-<<<<<<< HEAD
-    	var opts = {xaxis: {mode: 'time', minTickSize: [1, "day"]}, yaxis: {}, points: { show: true, radius: 10, fill: true }};
->>>>>>> parent of 38e8a8b... Flot-tooltips added
-=======
-=======
->>>>>>> e05a35bfad9300bac546f437f0ceb8e818061f27
-    	var opts = {xaxis: {mode: 'time', minTickSize: [1, "day"]}, yaxis: {}, points: { show: true, radius: 10, fill: true }};
->>>>>>> parent of 38e8a8b... Flot-tooltips added
-=======
-    	var opts = {xaxis: {mode: 'time', minTickSize: [1, "day"]}, yaxis: {}, points: { show: true, radius: 10, fill: true }};
->>>>>>> parent of 38e8a8b... Flot-tooltips added
 
     	var chart = $.plot(elem, [[0,0],[1,1]], opts);
 
